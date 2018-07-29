64bc8549481251ca5192ef92c55d57af6569bf0a
tank0123 <gim7646@naver.com>
2018-07-29 07:38:56 -0700

    first commit for pcap_test

--- main.cpp
+++ main.cpp
@@ -35,7 +35,7 @@ int main(int argc, char* argv[]) {
     if (res == -1 || res == -2) break;
     printf("%u bytes captured\n", header->caplen);
 
-    printPacket(packet, header->caplen);
+    printPacket(packet, header->caplen);	//function for print packet (packet content and header length is argument)
   }
 
   pcap_close(handle);
@@ -43,23 +43,23 @@ int main(int argc, char* argv[]) {
 }
 
 void printPacket(const u_char* _packet, long int len){
-	int i;
-	long long int src_port = 0;
-	long long int dst_port = 0;
-	long long int headerLength = 0;
-	long long int totalHLEN = 0;
+	int i;				//for loop 
+	long long int src_port = 0;	//for print src_port
+	long long int dst_port = 0;	//for print dst_prot
+	long long int headerLength = 0;	//for calculate tcp_headerLength
+	long long int totalHLEN = 0;	//for print content 16 bytes
 	
 	printf("dst_mac :\t");
 	for(i = 0; i < 6; i++){
 		if(i == 5){
 			if(_packet[i] < 16){
-				printf("0");
+				printf("0");	//if value is smaller than 10 print 0
 			}
 			printf("%X",_packet[i]);
 			break;
 		}
 		if(_packet[i] < 16){
-			printf("0");
+			printf("0");		//if value is smaller than 10 print 0
 		}
 		printf("%X:",_packet[i]);
 	}
@@ -69,19 +69,19 @@ void printPacket(const u_char* _packet, long int len){
         for(i = 6; i < 12; i++){
                 if(i == 11){
 			if(_packet[i] < 16){
-				printf("0");
+				printf("0");	//if value is smaller than 10 print 0
 			}
                         printf("%X",_packet[i]);
                         break;
                 }
 		if(_packet[i] < 16){
-			printf("0");
+			printf("0");		//if value is smaller than 10 print 0
 		}
                 printf("%X:",_packet[i]);
         }
         printf("\n");
 
-	totalHLEN += 14;
+	totalHLEN += 14;			//add 14bytes(ethernet Total Length)
 
 	if(_packet[12] == 0x08 && _packet[13] == 0x00){
 		//printf("%0X %0X %0X %0X\n",_packet[26],_packet[27],_packet[28],_packet[29]);
@@ -92,7 +92,6 @@ void printPacket(const u_char* _packet, long int len){
 				break;
 			}
 			printf("%lld.", _packet[i]);
-			//printf("%d.", strtoul(_packet[i], NULL, 16));
 		}
 		printf("\ndst_ip :\t");
 		for(i = 30; i < 34; i++){
@@ -104,14 +103,14 @@ void printPacket(const u_char* _packet, long int len){
 		}
 		printf("\n");
 
-		totalHLEN += 20;
+		totalHLEN += 20;			//add ip layer length
 			
-		src_port += _packet[34]<<8;
+		src_port += _packet[34]<<8;		//calculate src_port value
 		src_port += _packet[35];
 		
 		printf("src_port :\t%lld\n",src_port);
 
-		dst_port += _packet[36]<<8;
+		dst_port += _packet[36]<<8;		//calculate dst_prot value
 		dst_port += _packet[37];
 
 		printf("dst_port :\t%lld\n",dst_port);
@@ -120,11 +119,11 @@ void printPacket(const u_char* _packet, long int len){
 		headerLength = (long long int)_packet[46];
 		printf("HLEN :\t\t%lld\n", headerLength);
 
-		totalHLEN += headerLength;
+		totalHLEN += headerLength;		//add tcp header length
 
 		headerLength += 34;
 
-		if( (len - totalHLEN) >= 16){
+		if( (len - totalHLEN) >= 16){		// if total header length is bigger than totalHLEN print data content
 			printf("data content 16 bytes : ");
 			for(i = headerLength ; i < (headerLength+16) ; i++){
                         	printf("%X ",_packet[i]);
